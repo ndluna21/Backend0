3e1827895905e5ab33d5e844f16ffb9955e69203
ndluna21 <ducluan05102001@gmail.com>
2024-05-11 21:01:22 +0700

    commit lần 3

--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+require("dotenv").config();
+
+// import express from 'express'
+
 
 const app = express(); //app express
-const port = 3000; //port
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME;
 
-app.set("views", path.join(__dirname, './views/'))
+// Config template engine
+app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
- res.send("Hello World!");
+// Config static files
+app.use(express.static(path.join(__dirname, 'public'))); 
 
+// Khai báo route
+app.get("/", (req, res) => {
+ res.send("Hello World! My name is Luan");
 });
 
 app.get("/login", (req, res) => {
     res.render('example.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
  console.log(`Example app listening on port ${port}`);
 });
