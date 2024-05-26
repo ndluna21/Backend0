8dee612ebfd9e133f41b82a698f0f65e517b02ae
ndluna21 <ducluan05102001@gmail.com>
2024-05-26 09:06:44 +0700

    database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const configViewEngine = require('./config/viewEngine.js')
 const webRouter = require('./routes/web.js')
 
+const connection = require('./config/database.js')
+
 const app = express(); //app express
 const port = process.env.PORT || 8888; //port
 const hostname = process.env.HOST_NAME;
@@ -11,6 +13,17 @@ configViewEngine(app);
 
 // Khai báo route
 app.use('/', webRouter)
+
+// Test connection
+
+// A simple SELECT query
+connection.query(
+  'select * FROM Users u',
+  function (err, results, fields) {
+    console.log(">>>>>results= ", results); // results contains rows returned by server
+    //console.log(">>>>>fields= ", fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port,hostname, () => {
  console.log(`Example app listening on port ${port}`);
